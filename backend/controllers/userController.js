4090e9cd2138a322e8a43787a5350a383cac4682
rahulpalanivel <palanivelrahul45@gmail.com>
2024-03-17 01:11:26 +0530

    commit 5

---
+++ backend/controllers/userController.js
@@ -0,0 +1,68 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error("User aldready exist");
+  }
+
+  hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({
+    email: email,
+    password: hashedPassword,
+  });
+
+  if (newUser) {
+    res.status(201).json({
+      id: newUser.id,
+      email: newUser.email,
+    });
+  }
+
+  res.json({ message: "New User created" });
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Input all fields");
+  }
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          id: user.id,
+          email: user.email,
+        },
+      },
+      process.env.ACCESS_TOKEN_KEY,
+      { expiresIn: "5m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("email or Password not valid");
+  }
+});
+
+const currentUser = asyncHandler(async (req, res) => {
+  res.json(req.user);
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+  currentUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const connectdb = require("./config/dbConnection.js");
 const errorHandler = require("./middleware/errorHandler.js");
+
+connectdb();
+const app = express();
+
+const port = process.env.PORT || 4451;
+
+app.use(express.json());
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
